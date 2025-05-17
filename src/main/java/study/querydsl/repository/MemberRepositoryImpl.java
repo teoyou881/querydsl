a6c6e8deb919a4918386bbd8071a277f6c785b13
Teo_You <teoyou881@gmail.com>
2025-05-17 14:13:02 -0400

    create a custom repository with dsl

---
+++ src/main/java/study/querydsl/repository/MemberRepositoryImpl.java
@@ -0,0 +1,66 @@
+package study.querydsl.repository;
+
+import static study.querydsl.entity.QMember.member;
+import static study.querydsl.entity.QTeam.team;
+
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+import study.querydsl.dto.MemberSearchCondition;
+import study.querydsl.dto.MemberTeamDto;
+import study.querydsl.dto.QMemberTeamDto;
+
+@RequiredArgsConstructor
+public class MemberRepositoryImpl implements MemberRepositoryCustom {
+
+  private final JPAQueryFactory queryFactory;
+
+  @Override
+  public List<MemberTeamDto> search(MemberSearchCondition condition) {
+
+    return queryFactory.select(new QMemberTeamDto(
+                           member.id.as("memberId"),
+                           member.username,
+                           member.age,
+                           team.id.as("teamId"),
+                           team.name.as("teamName"))).from(member).leftJoin(member.team, team)
+                       // .where(
+                       //     usernameEq(condition.getUsername()),
+                       //     teamNameEq(condition.getTeamName()),
+                       //     ageGoe(condition.getAgeGoe()),
+                       //     ageLoe(condition.getAgeLoe()))
+                       .where(checkMemberDtoAll(condition)).fetch();
+  }
+
+  // StringUtils.hasText() -> 문자열이 null 이 아니고, 빈 문자열도 아니고, 공백으로만 이뤄지지 않는 경우 true
+  // 따라서 바로 null 체크 하지 말고, hasText 사용.
+  //
+
+  private BooleanExpression usernameEq(String username) {
+    return StringUtils.hasText(username) ? member.username.eq(username) : null;
+  }
+
+  private BooleanExpression teamNameEq(String teamName) {
+    return StringUtils.hasText(teamName) ? team.name.eq(teamName) : null;
+  }
+
+  private BooleanExpression ageGoe(Integer age) {
+    return age == null ? null : member.age.goe(age);
+  }
+
+  private BooleanExpression ageLoe(Integer age) {
+    return age == null ? null : member.age.loe(age);
+  }
+
+  private BooleanExpression checkMemberDtoAll(MemberSearchCondition condition) {
+    BooleanExpression username = usernameEq(condition.getUsername());
+    BooleanExpression teamName = teamNameEq(condition.getTeamName());
+    BooleanExpression ageGoe = ageGoe(condition.getAgeGoe());
+    BooleanExpression ageLoe = ageLoe(condition.getAgeLoe());
+
+    return Expressions.allOf(username, teamName, ageGoe, ageLoe);
+  }
+}
